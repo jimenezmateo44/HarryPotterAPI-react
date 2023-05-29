68201bfe47ed868ef216e3217dfa1ed523d0d4b3
Mateo Jimenez <mateojimenez37@gmail.com>
2023-05-29 15:10:43 -0300

    -- api harry potter --

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+// import Item from './MyItem';
+import React from 'react';
+
+class HarryPotter extends React.Component {
+  render() {
+    return (
+      <h1>Harry Potter API</h1>
+    )
+  }
+}
+
+class HarryPotterBody extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      nickname: null,
+      house: null,
+      actor: null,
+      image: null,
+    }
+  }
+
+  getNewCharacter() {
+    const randomNumber = Math.ceil(Math.random() * 23)
+    const url = `https://harry-potter-api.onrender.com/personajes/${randomNumber}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          name: data.personaje,
+          nickname: data.apodo,
+          house: data.casaDeHogwarts,
+          actor: data.interpretado_por,
+          image: data.imagen,
+          loadedCharacter: true,
+        })
+      })
+
+
+
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.state.loadedCharacter &&
+          <div>
+            <h1>Harry Potter Character's</h1>
+            <p>Name: {this.state.name}</p>
+            <p>Nickname: {this.state.nickname}</p>
+            <p>House: {this.state.house}</p>
+            <p>Actor: {this.state.actor}</p>
+            <p>Image: </p>
+            <img src={this.state.image}></img>
+
+          </div>
+        }
+
+        <button
+          type='button'
+          onClick={() => this.getNewCharacter()}
+          className="btn">Generate character
+        </button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HarryPotter />
+        <HarryPotterBody />
       </header>
     </div>
   );
